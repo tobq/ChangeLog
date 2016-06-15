e4e971eba59458d63357ff78e2e9ec4c0d47ad63
Tobi <Tobi.akiny@gmail.com>
2016-06-15 01:39:46 +0100

    form added

---
+++ app.js
@@ -0,0 +1,9 @@
+var http = require('http'),fs=require("fs");
+
+fs.readFile('./index.html', function (err, html) {
+    http.createServer(function(request, response) {
+        response.writeHeader(200, {"Content-Type": "text/html"});
+        response.write(html);
+        response.end();
+    }).listen(80);
+});
