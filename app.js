27bcfd42a337d9430482fd5de98cc758ea53ff51
Tobi <Tobi.akiny@gmail.com>
2016-06-18 14:27:50 +0100

    ok...

--- app.js
+++ app.js
@@ -1,23 +1,15 @@
 var express = require('express'),
     app = express(),
-    bodyParser = require('body-parser'),
-    fs=require("fs");
-app.use(bodyParser.urlencoded({ extended: true }));
+    bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-app.post('/', function(req, res) {
+app.post('/', function (req, res) {
     res.send(req.body);
-    console.log(req.body)
 });
-function add(p, callback) {
-    fs.readFile('.' + p, 'utf8', callback);
-}
 
 app.get(/./, function (req, res) {
-    path=(req.url === "/")?"/index.html":req.url ;
-    add(path, function (err, data) {
-        if (err) res.send("ERROR: 404 - Page Not Found")
-        res.send(data);
-    });
+    path = (req.url === "/") ? "/index.html" : "."+req.url;
+    res.sendFile(path,{"root": __dirname});
 })
 app.listen(80);
