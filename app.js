7a87e19ce58ed2eff02d7cb1b2495f0dfc7d0daf
Tobi <Tobi.akiny@gmail.com>
2016-06-18 20:15:08 +0100

    ok...

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express'),
     app = express(),
+    fs=require("fs"),
     bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
@@ -10,9 +11,13 @@ app.post('/', function (req, res) {
 });
 
 app.get(/./, function (req, res) {
-    path = (req.url === "/") ? "/index.html" : "."+req.url;
-    res.sendFile(path,{"root": __dirname});
+    path = (req.url === "/") ? "/index.html" : req.url;
+    fs.access("."+path, fs.F_OK, function(err) {
+        if (err) {
+            res.send("ERROR: 404 - (File Not Found)")
+        } else {
+            res.sendFile(path,{"root": __dirname});
+        }
+    });
 })
 app.listen(80);
-
-console.log(__dirname)
--- s.js
+++ s.js
@@ -4,8 +4,8 @@ function post() {
         type: "POST",
         data: {nm: $("input[name='nm']").val(), pw: $("input[name='pw']").val()},
         success: function (a, b, c) {
-            noti("posted", 1000);
             if (c.responseText==="false") noti("Invalid username or password");
+            else window.location+="dash"
         }
     })
     return false;
